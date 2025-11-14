4a36ef624c7cb04d8c77c0e7bc37fd9bfaf0cac9
Darkwebkilr <ismethaktan39@gmail.com>
2025-11-15 02:34:50 +0300

    teknolojiler

---
+++ src/components/techno/technos.js
@@ -0,0 +1,26 @@
+export const Techno = [
+  {
+    name: "Wordpress",
+    desc: "Web dünyasının uzun zamandır kullanılan popüler araçlarından biri olan <span>wordpress</span> güçlü yönetim paneli ile öne çıkıyor.",
+    img: "img/wordpress.png",
+    bg: "from-blue-500 via-indigo-500 to-purple-500",
+  },
+  {
+    name: "Next Js Ve React",
+    desc: "Web dünyasının uzun zamandır kullanılan popüler araçlarından biri olan <span>wordpress</span> güçlü yönetim paneli ile öne çıkıyor.",
+    img: "img/nextjs.png",
+    bg: "from-gray-900 via-gray-800 to-black",
+  },
+  {
+    name: "Photoshop",
+    desc: "Web dünyasının uzun zamandır kullanılan popüler araçlarından biri olan <span>wordpress</span> güçlü yönetim paneli ile öne çıkıyor.",
+    img: "img/photoshop.png",
+    bg: "from-blue-400 via-indigo-500 to-pink-500",
+  },
+  {
+    name: "Next Js",
+    desc: "Web dünyasının uzun zamandır kullanılan popüler araçlarından biri olan <span>wordpress</span> güçlü yönetim paneli ile öne çıkıyor.",
+    img: "img/nextjs.png",
+    bg: "from-black via-gray-900 to-blue-600",
+  },
+];
---
+++ src/scripts/techno.js
@@ -0,0 +1,43 @@
+import { gsap } from "gsap";
+import { SplitText } from "gsap/all";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+gsap.set("#heading", { opacity: 0 });
+let split = SplitText.create("#heading", { type: "chars" });
+
+ScrollTrigger.create({
+  trigger: "#heading",
+  once: true,
+  onEnter: () => {
+    gsap.set("#heading", { opacity: 1 });
+    gsap.from(split.chars, {
+      duration: 0.5,
+      y: 20,
+      autoAlpha: 0,
+      stagger: {
+        amount: 0.5,
+        from: "random",
+        yoyo: true,
+      },
+    });
+  },
+});
+
+gsap.utils.toArray("#tt").forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "top top",
+    pin: true,
+    pinSpacing: false,
+  });
+});
+
+gsap.utils.toArray("#tt").forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    snap: 1 / 2,
+    duration: 0.1,
+  });
+});
